bb48fd5b03352f12f38901be9c6e938ea426c34b
Kristen Shea <kstar61@live.com>
2018-09-03 17:19:19 -0500

    calculate tip from two user-entered variables

--- tip calculator/Program.cs
+++ tip calculator/Program.cs
@@ -6,7 +6,40 @@ namespace tip_calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Tip Calculator");
+
+            //ask for check subtotal then convert to decimal
+            Console.Write("Enter subtotal:  ");
+            decimal subtotal = decimal.Parse(Console.ReadLine());
+
+            //prompt for percentage tip user would like to leave and convert to decimal
+            Console.Write("What percentage would you like to tip?  ");
+            decimal tip = decimal.Parse(Console.ReadLine());
+
+            //multiply check subtotal by percent, already converted to decimal in method below
+            decimal tipAmount = subtotal * PercentTip(tip);
+            
+            //convert tipAmount to a string formatted as currency
+            string tipAmountString = $"{tipAmount:C}";
+
+            //print tip amount to console
+            Console.WriteLine("You should leave {0}.", tipAmountString);
+
+            Console.ReadLine();
+
         }
+
+        /// <summary>
+        /// method to convert percent entered above to decimal to figure out tip
+        /// </summary>
+        /// <param name="tip"></param>
+        /// <returns>percentage as a decimal, ex 25 will return as .25</returns>
+        private static decimal PercentTip(decimal tip)
+        {
+            return tip / 100;
+        }
+
+
     }
 }
